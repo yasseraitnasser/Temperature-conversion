3bc54e57c28e44a36dd9437f5701ece0becb724f
yait-nas <yait-nas@c3r10p5.1337.ma>
2025-11-30 14:21:19 +0100

    done

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
+const temperature = document.getElementById("Temperature");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+
+let temp;
+
+function convert() {
+    if (toFahrenheit.checked)
+    {
+        temp = Number(temperature.value) * 9 / 5 + 32;
+        result.textContent = temp;
+    }
+    else if (toCelsius.checked)
+    {
+        temp = (Number(temperature.value) - 32) * 5 / 9;
+        result.textContent = temp;
+    }
+    else
+    {
+        window.alert(`Please select a unit`);
+    }
+}
 
